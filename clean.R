c0492b1ec9561aa67be03332f8dea036db08c2dd
nicolewhite <nicole.white@neotechnology.com>
2014-11-10 21:07:30 -0800

    Add data source as comment.

--- clean.R
+++ clean.R
@@ -1,7 +1,8 @@
 options(stringsAsFactors=F)
-complaints = read.csv("Consumer_Complaints.csv")
-
 library(dplyr)
+
+# Consumer_Complaints.csv downloaded from http://catalog.data.gov/dataset/consumer-complaint-database
+complaints = read.csv("Consumer_Complaints.csv")
 
 # Remove unnecessary columns.
 complaints = select(complaints, -ZIP.code, -Submitted.via, -Date.sent.to.company)
@@ -29,7 +30,7 @@ complaints$DateReceived = as.Date(complaints$DateReceived, format = "%m/%d/%y")
 complaints$ConsumerDisputed = ifelse(complaints$ConsumerDisputed == "Yes", "Yes", "No")
 
 # Write clean data.
-write.csv(complaints, file = "consumer_complaints_304k.csv", row.names = F)
+write.csv(complaints, file = "consumer_complaints_310k.csv", row.names = F)
 
 medium = complaints[sample(1:nrow(complaints), 100000), ]
 small = complaints[sample(1:nrow(complaints), 30000), ]
